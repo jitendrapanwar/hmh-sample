99ff6984d64fad36626cc6cb8313c5fe1fa53ed5
Jitendra Panwar <jitendra.panwar@hurix.com>
2016-03-07 11:44:03 +0530

    whole file contents are replaced

--- HeaderModal.js
+++ HeaderModal.js
@@ -1,45 +1,21 @@
-import React, { PropTypes } from 'react';
-import Modal from 'react-bootstrap/lib/Modal';
-import OverviewContent from './OverviewContent';
-import { connect } from 'react-redux';
-import BackgroundContent from './BackgroundContent';
-import LabReportContent from './LabReportContent';
-import ChallengeContent from './ChallengeContent';
-import { tabTitles } from '../components/TabList';
+import React from 'react';
 
-const getModalTitle = (props) => {
-  return (props.tabs.active) < 0 ? 'untitled' : tabTitles[props.tabs.active].titleName;
-};
+/* custom */
+import ActivityContainer from './ActivityContainer';
+import HeaderTabs from '../containers/HeaderTabs';
+import HeaderPopup from '../containers/HeaderPopup';
+import ToolBox from '../components/ToolBox';
+import '../style/override.scss';
 
-return (props.tabs.active) < 0 ? 'untitled' : tabTitles[props.tabs.active].title;
-function displayNavNModal(index) {
-  const modelData = ModalContent.content[0];
-  switch (index) {
-    case 0:
-      return <OverviewContent content={modelData} />;
-    case 1:
-      return <BackgroundContent content={modelData} />;
-    default:
-      return <OverviewContent content={modelData} />;
-  }
-}
-
-const HeaderModal = (props) => (
-  <Modal id="navModal"
-    {...props}
-  >
-    <Modal.Header closeButton>
-      <Modal.Title>{getModalTitle(props)}</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      {displayNavNModal(props.tabs.active)}
-    </Modal.Body>
-  </Modal>
+const App = () => (
+  <div>
+    <HeaderTabs>Haber Process</HeaderTabs>
+    <div className="content">
+      <HeaderPopup />
+      <ActivityContainer/>
+      <ToolBox/>
+    </div>
+  </div>
 );
 
-function mapStateToProps({ tabs }) {
-  return {
-    tabs,
-  };
-}
-export default connect(mapStateToProps)(HeaderModal);
+export default App;
