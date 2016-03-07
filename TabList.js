bbca0d6cc4f20d689aebefcaa8c5d30dcc784a9d
Jitendra Panwar <jitendra.panwar@hurix.com>
2016-03-07 12:22:13 +0530

    remove Tablist.js

--- TabList.js
+++
@@ -1,7 +0,0 @@
-import { OVERVIEW, BACKGROUND, LAB_REPORT, CHALLENGE } from '../constants/const';
-export const tabTitles = [
-    { title: OVERVIEW },
-    { title: BACKGROUND },
-    { title: LAB_REPORT },
-    { title: CHALLENGE },
-];
